1858e9241df4292acfaf99cff936aa46e4e4ab97
Chipes12 <carlos.andres_paez@outlook.com>
2022-08-27 17:14:37 -0500

    ready to go

--- main.js
+++ main.js
@@ -2,13 +2,14 @@ const { app, BrowserWindow } = require("electron");
 
 app.whenReady().then(() => {
     const window = new BrowserWindow({
+        icon: 'build/icono.ico',
         width: 500,
-        height: 500,
+        height: 300,
         resizable: true,
         webPreferences: {
             nodeIntegration: true,
             contextIsolation: false,
         }
     });
-    window.loadFile("./window.html");
+    window.loadFile("./assets/window.html");
 })
